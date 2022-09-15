ff3b97b79ffab991bd9f980d5fa91295a357e230
Ltfyysr <lutfiyeyasar38@gmail.com>
2022-09-15 22:57:42 +0300

    cart_page added

---
+++ lib/cubit/cart_page_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:foodie/entity/sepet_yemekler.dart';
+import 'package:foodie/entity/yemekler.dart';
+import 'package:foodie/repo/sepetyemeklerdao_repository.dart';
+
+class CartPageCubit extends Cubit<List<SepetYemekler>>{
+
+  CartPageCubit():super(<SepetYemekler>[]);
+  var srepo = SepetYemeklerDaoRepository();
+
+  Future<void> addToSepetWithoutCheck(
+      Yemekler yemek,  int yemek_siparis_adet) async {
+    await srepo.addToSepetWithoutCheck(yemek, yemek_siparis_adet);
+  }
+  /*Future<void> add(Yemekler yemek,int yemek_siparis_adet) async{
+   await srepo.addToSepet(yemek, yemek_siparis_adet);
+  }*/
+
+  Future<void> sepettekileriGetir() async {
+    var liste = await srepo.getBasket();
+    emit(liste);
+  }
+
+}
