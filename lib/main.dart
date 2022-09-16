2a5a459a57d261d2fd03b5f32982d6ece54987ce
Ltfyysr <lutfiyeyasar38@gmail.com>
2022-09-16 19:45:17 +0300

    firebase transactions done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:foodie/colors.dart';
@@ -6,7 +7,9 @@ import 'package:foodie/views/landing_page.dart';
 
 import 'cubit/cart_page_cubit.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
