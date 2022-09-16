7ac73f41ada136bc3e73d79276ad779fe3f4feb4
Ltfyysr <lutfiyeyasar38@gmail.com>
2022-09-16 23:15:13 +0300

    user login

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,5 +1,7 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:foodie/colors.dart';
+import 'package:foodie/service/auth.dart';
 import 'package:foodie/views/home_page.dart';
 import 'package:foodie/views/register_page.dart';
 
@@ -11,8 +13,11 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  var tfUserName = TextEditingController();
+  var tfEmail = TextEditingController();
   var tfPassword = TextEditingController();
+
+  AuthService _authService= AuthService();
+
 
   @override
   Widget build(BuildContext context) {
@@ -31,13 +36,13 @@ class _LoginPageState extends State<LoginPage> {
               Padding(
                 padding:
                 ProjectPaddings.textFieldPadding,
-                  child: TextField(controller: tfUserName,
+                  child: TextField(controller: tfEmail,
                     decoration: InputDecoration(
-                      hintText: "UserNname",
+                      hintText: "Email",
                       hintStyle: TextStyle(color: color5),
                       filled: true,
                       fillColor: color4,
-                      icon: Icon(Icons.person),
+                      icon: Icon(Icons.mail),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20),
                       ),
@@ -98,10 +103,52 @@ class _LoginPageState extends State<LoginPage> {
                     height: 50,
                     child: ElevatedButton(
                       onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>const HomePage()));
+                        if (tfEmail.text.isEmpty ||
+                          tfPassword.text.isEmpty){
+                        AwesomeDialog(
+                            context: context,
+                            dialogType: DialogType.ERROR,
+                            title: "Warning!",
+                            desc: "Please fill in all fields",
+                            btnOkColor: color8,
+                            btnOkOnPress: () {})
+                          .show();
+
+                      } else if (tfPassword.text.length <8 ) {
+                        AwesomeDialog(
+                            context: context,
+                            dialogType: DialogType.ERROR,
+                            animType: AnimType.SCALE,
+                            title: "Error!",
+                            desc: "The password you enter must be at least 8 characters long!",
+                            btnOkColor: color8,
+                            btnOkOnPress: () {Navigator.pop(context);})
+                          .show();
+                      } else {
+                        if (!emailValid(tfEmail.text)) {
+                          AwesomeDialog(
+                              context: context,
+                              dialogType: DialogType.ERROR,
+                              animType: AnimType.SCALE,
+                              title: "Warning!",
+                              desc: "Please enter a valid email!",
+                              btnOkColor: color8,
+                              btnOkOnPress: () {Navigator.pop(context);})
+                            .show();
+                        } else {
+                          showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (context) {
+                                return Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              });
+                            _authService.signIn(tfEmail.text, tfPassword.text).then((value){
+                              return Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()),);
+                            });
+                         }}
+
                       },
                       child: const Text("Login"),
                       style: ElevatedButton.styleFrom(
@@ -116,4 +163,12 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
+
+  bool emailValid(String email) {
+    return RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(email);
+  }
 }
+
+
