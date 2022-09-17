1ae0c9326504380ed203fb1b9d6f67828cbe2bb9
Ltfyysr <lutfiyeyasar38@gmail.com>
2022-09-17 17:36:48 +0300

    login completed

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,7 +1,9 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:foodie/colors.dart';
-import 'package:foodie/service/auth.dart';
+import 'package:foodie/entity/users.dart';
 import 'package:foodie/views/home_page.dart';
 import 'package:foodie/views/register_page.dart';
 
@@ -15,8 +17,6 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   var tfEmail = TextEditingController();
   var tfPassword = TextEditingController();
-
-  AuthService _authService= AuthService();
 
 
   @override
@@ -34,24 +34,25 @@ class _LoginPageState extends State<LoginPage> {
                   child: Image.asset("images/login.png")),
               SizedBox(height: 20),
               Padding(
-                padding:
-                ProjectPaddings.textFieldPadding,
-                  child: TextField(controller: tfEmail,
-                    decoration: InputDecoration(
-                      hintText: "Email",
-                      hintStyle: TextStyle(color: color5),
-                      filled: true,
-                      fillColor: color4,
-                      icon: Icon(Icons.mail),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                    ),),
+                padding: ProjectPaddings.textFieldPadding,
+                child: TextField(
+                  controller: tfEmail,
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    hintStyle: TextStyle(color: color5),
+                    filled: true,
+                    fillColor: color4,
+                    icon: Icon(Icons.mail),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
               ),
               Padding(
-                padding:
-                ProjectPaddings.textFieldPadding,
-                  child: TextField( obscureText: true,
+                  padding: ProjectPaddings.textFieldPadding,
+                  child: TextField(
+                    obscureText: true,
                     controller: tfPassword,
                     decoration: InputDecoration(
                       hintText: "Password",
@@ -62,8 +63,8 @@ class _LoginPageState extends State<LoginPage> {
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20),
                       ),
-                    ),)
-              ),
+                    ),
+                  )),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
@@ -83,10 +84,10 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                   ElevatedButton(
                     onPressed: () {},
-                    child:  Text(
+                    child: Text(
                       "Forgot Password?",
-                      style: TextStyle(
-                          color: color3, fontWeight: FontWeight.bold),
+                      style:
+                          TextStyle(color: color3, fontWeight: FontWeight.bold),
                     ),
                     style: ElevatedButton.styleFrom(
                         primary: anaRenk,
@@ -103,52 +104,43 @@ class _LoginPageState extends State<LoginPage> {
                     height: 50,
                     child: ElevatedButton(
                       onPressed: () {
-                        if (tfEmail.text.isEmpty ||
-                          tfPassword.text.isEmpty){
-                        AwesomeDialog(
-                            context: context,
-                            dialogType: DialogType.ERROR,
-                            title: "Warning!",
-                            desc: "Please fill in all fields",
-                            btnOkColor: color8,
-                            btnOkOnPress: () {})
-                          .show();
-
-                      } else if (tfPassword.text.length <8 ) {
-                        AwesomeDialog(
-                            context: context,
-                            dialogType: DialogType.ERROR,
-                            animType: AnimType.SCALE,
-                            title: "Error!",
-                            desc: "The password you enter must be at least 8 characters long!",
-                            btnOkColor: color8,
-                            btnOkOnPress: () {Navigator.pop(context);})
-                          .show();
-                      } else {
-                        if (!emailValid(tfEmail.text)) {
+                        if (tfEmail.text.isEmpty || tfPassword.text.isEmpty) {
+                          AwesomeDialog(
+                                  context: context,
+                                  dialogType: DialogType.ERROR,
+                                  title: "Warning!",
+                                  desc: "Please fill in all fields",
+                                  btnOkColor: color8,
+                                  btnOkOnPress: () {})
+                              .show();
+                        } else if (tfPassword.text.length < 8) {
                           AwesomeDialog(
                               context: context,
                               dialogType: DialogType.ERROR,
                               animType: AnimType.SCALE,
-                              title: "Warning!",
-                              desc: "Please enter a valid email!",
+                              title: "Error!",
+                              desc:
+                                  "The password you enter must be at least 8 characters long!",
                               btnOkColor: color8,
-                              btnOkOnPress: () {Navigator.pop(context);})
-                            .show();
+                              btnOkOnPress: () {
+                                Navigator.pop(context);
+                              }).show();
                         } else {
-                          showDialog(
-                              context: context,
-                              barrierDismissible: false,
-                              builder: (context) {
-                                return Center(
-                                  child: CircularProgressIndicator(),
-                                );
-                              });
-                            _authService.signIn(tfEmail.text, tfPassword.text).then((value){
-                              return Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()),);
-                            });
-                         }}
-
+                          if (!emailValid(tfEmail.text)) {
+                            AwesomeDialog(
+                                context: context,
+                                dialogType: DialogType.ERROR,
+                                animType: AnimType.SCALE,
+                                title: "Warning!",
+                                desc: "Please enter a valid email!",
+                                btnOkColor: color8,
+                                btnOkOnPress: () {
+                                  Navigator.pop(context);
+                                }).show();
+                          } else {
+                            signIn();
+                          }
+                        }
                       },
                       child: const Text("Login"),
                       style: ElevatedButton.styleFrom(
@@ -164,11 +156,54 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
+  Future signIn() async {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        });
+
+    try {
+     await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: tfEmail.text.trim(),
+          password: tfPassword.text.trim());
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+              builder: (context) => HomePage()),
+              (Route<dynamic> route) => false);
+    }  catch (e) {
+      AwesomeDialog(
+              context: context,
+              dialogType: DialogType.ERROR,
+              animType: AnimType.SCALE,
+              title: "Warning!",
+              desc: "You entered incorrect information",
+              btnOkColor: color8,
+              btnOkOnPress: () {Navigator.pop(context);})
+          .show();
+    }
+  }
+
+  Stream<Users> readUser(String e) => FirebaseFirestore.instance
+      .collection("users")
+      .snapshots()
+      .map((snapshot) => snapshot.docs
+      .map((doc) => Users.fromJson(doc.data()))
+      .toList()
+      .firstWhere((eachElement) => eachElement.email == e, orElse: () {
+    return Users(
+        email: tfEmail.text,
+        userName: "",
+        timeStamp: DateTime.now());
+  }));
+
   bool emailValid(String email) {
     return RegExp(
-        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
         .hasMatch(email);
   }
 }
-
-
